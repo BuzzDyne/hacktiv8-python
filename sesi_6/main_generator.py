aa581f61ed1fdb78a87e33515520665a79ea2ce9
BuzzDyne <buzzdyne@gmail.com>
2021-11-11 09:09:07 +0700

    Added test for generator's next()

--- sesi_6/main_generator.py
+++ sesi_6/main_generator.py
@@ -8,8 +8,15 @@ def counter(low, high=None):
             yield low
             low += 1
 
+print(type(counter(1)))
 
-for x in counter(1):
+for x in counter(1,3):
     # if x % 20 == 0:
     #     print()
     print(x, end=" ")
+
+x = counter(1,3)
+
+print(next(x))
+print(next(x))
+print(next(x))
